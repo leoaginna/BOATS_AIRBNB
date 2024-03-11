daebf7923fc6d21405aec6a5b785a39d8a0b6e0d
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-11 17:11:34 -0300

    add rentals controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,16 +1,49 @@
 class RentalsController < ApplicationController
+  def index
+    @rentalss = Rental.all
+  end
+
+  def show
+    @rental = Rental.find params[:id]
+  end
+
   def create
+    @rental = Rental.new rental_params
+    @rental.user = current_user
+    @rental.boat = @boat
   end
 
-  def new
-  end
-
-  def edit
+  def create_review
+    @rental = Rental.find params[:id]
+    @rental.review = review_params[:review]
+    if @rental.save
+      redirect_to @rental, status: :see_others
+    else
+      render rental_path
+    end
   end
 
   def update
+    @rental = Rental.find params[:id]
+    if @rental.update(rental_params)
+      redirect_to @rental, status: :see_others
+    else
+      render rental_path
+    end
   end
 
   def cancel
+    @rental = Rental.find params[:id]
+    @rental.available = false
+  end
+
+  private
+
+  def review_params
+    params.require(:rental).permit(:review)
+  end
+
+  def rental_params
+    params.require(:rental).permit(:start_time, :end_time)
   end
 end
