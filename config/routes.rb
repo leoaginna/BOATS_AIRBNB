c7835a41ce709f749860a283c159ab3ad0d64fd8
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-11 16:14:03 -0300

    changed delete for cancel

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get 'rentals/create'
+  get 'rentals/new'
+  get 'rentals/edit'
+  get 'rentals/update'
+  get 'rentals/cancel'
+  get 'boats/index'
+  get 'boats/new'
+  get 'boats/create'
+  get 'boats/edit'
+  get 'boats/update'
+  get 'boats/delete'
   devise_for :users
   root "boats#index"
   resources :boats, except: :index do
-    resources :rentals, only: [:new, :create, :edit, :update, :delete]
+    resources :rentals, only: [:new, :create, :edit, :update, :cancel]
   end
   resources :rentals, only: [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_11_190009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "boats", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.string "address"
+    t.boolean "available"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.date "start_time"
+    t.date "end_time"
+    t.text "review"
+    t.bigint "boats_id", null: false
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boats_id"], name: "index_rentals_on_boats_id"
+    t.index ["users_id"], name: "index_rentals_on_users_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "rentals", "boats", column: "boats_id"
+  add_foreign_key "rentals", "users", column: "users_id"
 end
