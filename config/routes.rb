08d968323abf48ced06b2e13425bb3f4a2c1b91d
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-11 17:31:20 -0300

    deleted new rental action (lets just embed it)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "boats#index"
   resources :boats, except: :index do
-    resources :rentals, only: [:new, :create]
+    resources :rentals, only: [:create]
   end
   resources :rentals, only: [:index, :show, :edit, :update, :cancel]
 end
