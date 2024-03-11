cbf5959ae44f405cf1d33bf70a09bb72b02558f0
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-11 16:30:11 -0300

    corrected route logic again

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,8 @@
 Rails.application.routes.draw do
-  get 'rentals/create'
-  get 'rentals/new'
-  get 'rentals/edit'
-  get 'rentals/update'
-  get 'rentals/cancel'
-  get 'boats/index'
-  get 'boats/new'
-  get 'boats/create'
-  get 'boats/edit'
-  get 'boats/update'
-  get 'boats/delete'
   devise_for :users
   root "boats#index"
   resources :boats, except: :index do
-    resources :rentals, only: [:new, :create, :edit, :update, :cancel]
+    resources :rentals, only: [:new, :create]
   end
-  resources :rentals, only: [:index, :show]
+  resources :rentals, only: [:index, :show, :edit, :update, :cancel]
 end
