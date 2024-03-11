0348ce2827cd8bbb69feaccdecf1da7c560260be
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-11 16:03:25 -0300

    generated devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "boats#index"
   resources :boats, except: :index do
     resources :rentals, only: [:new, :create, :edit, :update, :delete]
