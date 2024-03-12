0fab9794c98513312a1b22a3c7bae876586a36fb
ginna velasco <ginna.velascop@gmail.com>
2024-03-12 12:43:33 -0300

    added my_boats routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "boats#index"
-  resources :boats, except: :index do
+  resources :boats do
     resources :rentals, only: [:create]
   end
   resources :rentals, only: [:index, :show, :edit, :update, :cancel]
+
+  get "/myboats", to: "boats#my_boats", as: "myboats"
 end
