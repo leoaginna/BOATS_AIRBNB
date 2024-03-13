b52510ec28e375e79d4b3de2cc75adf56e2e47ca
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-13 10:16:06 -0300

    removed rental show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "boats#index"
   resources :boats, except: :index
-  resources :rentals, only: [:index,:create, :show, :update]
+  resources :rentals, only: [:index,:create, :update]
   get "/myboats", to: "boats#my_boats", as: "myboats"
 end
