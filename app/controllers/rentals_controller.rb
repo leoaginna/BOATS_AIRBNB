1c31421ff1c16fbdd4e1bfc9373b533fe8631f74
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-14 15:56:07 -0300

    cancelled rentals move to history fix

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,6 +1,6 @@
 class RentalsController < ApplicationController
   def index
-    @my_rentals = Rental.where("user_id = ?")
+    @my_rentals = Rental.where("user_id = ?", current_user.id)
     @past_rentals = @my_rentals.select { |rental| rental.end_time < Date.today || rental.state == 'cancelled'}
     @current_rentals = @my_rentals.select { |rental| rental.end_time >= Date.today && rental.state != 'cancelled'}
   end
