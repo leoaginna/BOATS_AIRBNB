32be795624de064c09552090bd327c29764006a7
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-14 16:54:36 -0300

    improved seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 require 'faker'
+require 'cloudinary'
+require 'open-uri'
 
-puts 'Creating a boat...'
-
-10.times do
-  puts 'Creating a boat...'
+100.times do
+  p "creating 1 stuff..."
   user = User.new(
     email: Faker::Internet.email,
     password: Faker::Internet.password
@@ -15,8 +15,29 @@ puts 'Creating a boat...'
     price: Faker::Number.decimal(l_digits: 2),
     available: Faker::Boolean.boolean,
     address: Faker::Address.full_address,
-    user_id: 1,
-    # image: Faker::LoremFlickr.image
+    available_from: Faker::Date.between(from: 2.days.ago, to: 2.days.from_now),
+    available_until: Faker::Date.between(from: 2.days.from_now, to: 1.month.from_now),
+    user_id: user.id
   )
+
+  # Upload Cloudinary images
+
   boat.save!
+
+  3.times do
+    image_url = Faker::LoremFlickr.image(size: "500x300", search_terms: ['boat'])
+    io = URI.open(image_url)
+
+    boat.photos.attach(io:, filename: "boat_#{Time.now.to_i}.jpg", content_type: 'image/jpg')
+  end
+
+  10.times do
+    rental = Rental.new(
+      start_time: Faker::Date.between(from: 2.days.ago, to: 2.days.from_now),
+      end_time: Faker::Date.between(from: 2.days.from_now, to: 1.month.from_now),
+      user_id: user.id,
+      boat_id: boat.id
+    )
+    rental.save!
+  end
 end
